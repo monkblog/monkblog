51fa2b2f2391c0bedec139a2ff230eebf8568ee6
Ben Overmyer <manatrance@gmail.com>
2014-11-21 08:31:16 -0600

    Fix issue #2, changing body column type to TEXT

---
+++ app/database/migrations/2014_11_21_080314_update_posts_and_pages_with_long_text.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdatePostsAndPagesWithLongText extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table( 'posts', function( Blueprint $table ) {
+			DB::statement( 'ALTER TABLE posts MODIFY COLUMN body TEXT');
+		});
+
+		Schema::table( 'pages', function( Blueprint $table ) {
+			DB::statement( 'ALTER TABLE posts MODIFY COLUMN body TEXT');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table( 'posts', function( Blueprint $table ) {
+			DB::statement( 'ALTER TABLE posts MODIFY COLUMN body VARCHAR(255)');
+		});
+
+		Schema::table( 'pages', function( Blueprint $table ) {
+			DB::statement( 'ALTER TABLE posts MODIFY COLUMN body VARCHAR(255)');
+		});
+	}
+
+}
