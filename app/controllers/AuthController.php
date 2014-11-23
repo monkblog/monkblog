3a30f87a2fc7409a38881060c639872af0762bee
Eric Dowell <eric@d0we11.com>
2014-11-23 17:12:33 -0600

    Closes #14, adds login errors and redirect to internal admin/* if attempted before auth'd

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,29 +1,49 @@
 <?php
 
+use Illuminate\Support\MessageBag;
+
 class AuthController extends BaseController {
 
     public function getLogin() {
-
-        $email = Input::get( 'email' , null );
+        $errors = ( Input::old( 'errors' ) ) ? Input::old( 'errors' ) : new MessageBag();
 
         $viewData = [
             'pageTitle' => 'Login',
-            'email' => $email,
+            'errors' => $errors,
+            'redirect' => Input::get( 'redirect_to' ),
         ];
-
         return Response::view( 'login', $viewData );
     }
 
     public function postLogin() {
-
         $email = Input::get( 'email' );
-        $password = Input::get( 'password' );
 
-        if ( Auth::attempt( [ 'email' => $email, 'password' => $password ] ) ) {
-            return Redirect::route( 'admin.home' );
-        } else {
-            return Redirect::route( 'login' )->withInput( [ 'email' => $email  ] );
+        $validator = Validator::make( Input::all(), [
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+
+        if( $validator->passes() ) {
+            $credentials = [
+                'email' => $email,
+                'password' => Input::get( 'password' )
+            ];
+            if( Auth::attempt( $credentials ) ) {
+                if( $redirect_admin = Input::get( 'redirect_to' ) ) {
+                    return Redirect::to( $redirect_admin );
+                }
+                return Redirect::route( 'admin.home' );
+            }
+            $data[ 'errors' ] = new MessageBag([
+               'password' => 'Email and/or Password are invalid',
+            ]);
         }
+        else {
+            $data[ 'errors' ] = $validator->getMessageBag();
+        }
+        $data[ 'email' ] = $email;
+
+        return Redirect::route( 'login' )->withInput( $data );
     }
 
 
--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -1,11 +1,20 @@
 @extends('layout')
 
 @section('content')
-    {{ Form::open( [ 'route' => 'login' ] ) }}
+    {{ Form::open( [ 'route' => 'login', 'required' ] ) }}
         {{ Form::label( 'email', 'Email' ) }}
-        {{ Form::email( 'email', $email ) }}
+        {{ Form::email( 'email', Input::old( 'email' ), [ 'required' ] ) }}
+        @if( $error = $errors->first( 'email' ) )
+        <small class="error">{{ $error }}</small>
+        @endif
         {{ Form::label( 'password', 'Password' ) }}
         {{ Form::password( 'password' , [ 'required' ] ) }}
+        @if( $error = $errors->first( 'password' ) )
+        <small class="error">{{ $error }}</small>
+        @endif
+        @if( $redirect )
+        {{ Form::hidden( 'redirect_to' , $redirect ) }}
+        @endif
         {{ Form::submit( 'Login', [ 'class' => 'button' ] ) }}
     {{ Form::close() }}
 @stop
