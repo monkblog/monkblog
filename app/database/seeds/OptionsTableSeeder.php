117054dc3254ed25063fe728e9284744ccfe1d91
Eric Dowell <eric@d0we11.com>
2015-01-02 09:23:27 -0600

    Small change allows for new entries to be easily seeded to an OptionTab, such as maybe github in contact info

--- app/database/seeds/OptionsTableSeeder.php
+++ app/database/seeds/OptionsTableSeeder.php
@@ -7,28 +7,28 @@ class OptionsTableSeeder extends Seeder {
         $optionTabs = Config::get( 'option_tabs' , [] );
 
         foreach( $optionTabs as $tab ) {
-            $checkOptionTab = DB::table( 'option_tabs' )->where( 'slug', $tab[ 'slug' ] )->get();
+            $optionTab = OptionTab::where( 'slug', $tab[ 'slug' ] )->get()->first();
 
-            if( empty( $checkOptionTab ) ) {
+            if( empty( $optionTab ) ) {
                 $optionTab = new OptionTab;
                 $optionTab->slug = $tab[ 'slug' ];
                 $optionTab->display_name = ucwords( str_replace( '_', ' ', $tab[ 'slug' ] ) );
 
                 $optionTab->save();
+            }
 
-                foreach( $tab[ 'options' ] as $name => $value ) {
-                    $checkOption = DB::table( 'options' )->where( 'name', $name )->get();
+            foreach( $tab[ 'options' ] as $name => $value ) {
+                $checkOption = Option::where( 'name', $name )->get()->first();
 
-                    if( empty( $checkOption ) && !empty( $value ) ) {
-                        $option = new Option;
-                        $option->name = $name;
-                        $option->display_name = ucwords( str_replace( '_', ' ', $name ) );
-                        $option->value = $value;
-                        $option->autoload = true;
-                        $option->sort_order = 1;
+                if( empty( $checkOption ) && !empty( $value ) ) {
+                    $option = new Option;
+                    $option->name = $name;
+                    $option->display_name = ucwords( str_replace( '_', ' ', $name ) );
+                    $option->value = $value;
+                    $option->autoload = true;
+                    $option->sort_order = 1;
 
-                        $optionTab->options()->save( $option );
-                    }
+                    $optionTab->options()->save( $option );
                 }
             }
         }
